9ba23b6b855389d0afca542bdfa20c0293543179
RamNathan691 <ramnathan0691@gmail.com>
2019-08-12 09:50:02 +0530

    finally done with displaying the images in the website

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title=models.CharField(max_length=50)
-    descrpition=models.CharField(max_length=1000)
-    price=models.DecimalField(max_digits=5, decimal_places=2,default=0.0)
-
+    descrpition=models.TextField(max_length=1000)
+    price=models.DecimalField(max_digits=10, decimal_places=2,default=0.0)
+    image=models.ImageField(upload_to="products/",null=True,blank=True)
+    def __str__(self):
+        return self.title
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+#from django.views import ListView
+from . models import Product
+from django.views.generic import ListView,DetailView
 
-# Create your views here.
+
+
+class ProductListView(ListView):
+    queryset=Product.objects.all()
+    template_name='products/productslist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView,self).get_context_data(**kwargs)
+       
+        return context
+    
+
+class ProductDetailView(DetailView):
+    queryset=Product.objects.all()
+    template_name='products/productsdetail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView,self).get_context_data(**kwargs)
+        print(context)
+        return context
+        
