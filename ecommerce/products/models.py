3d5d4c6eca3e1edbef60ddab23eb33d20c73e613
RamNathan691 <ramnathan0691@gmail.com>
2019-08-16 17:37:17 +0530

    Done with the reverse URL

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_save
+from django.urls import reverse
 # Create your models here.
 from .utils import unique_slug_generator
 class ProductManager(models.Manager):
@@ -24,7 +25,8 @@ class Product(models.Model):
     objects=ProductManager()  
     
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        #return "/products/{slug}/".format(slug=self.slug)
+         return reverse("productsdetailfeatured",kwargs={"slug":self.slug} )
 
 def product_presave(sender,instance,**args):
     if not instance.slug:
