b3f4fa3c90b0242a1dd6ff9b39bdabe5ea2211bb
RamNathan691 <ramnathan0691@gmail.com>
2019-08-18 12:37:53 +0530

    Got an idea and executed it successfully mesages

--- ecommerce/search/views.py
+++ ecommerce/search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from products.models import Product
+from django.contrib import messages
 from django.db.models import Q #just a letter Q
 
 # Create your views here.
@@ -19,5 +20,8 @@ class SearchProductView(ListView):
         query = method_dict.get('q',None)
         if query is not None:
             lookups =Q(title__icontains=query)|Q(description__icontains=query)|Q(price__icontains=query)
-            return Product.objects.filter(lookups).distinct()#used to remove the redundant product
+            if Product.objects.filter(lookups):
+                return Product.objects.filter(lookups).distinct()
+            else:    #used to remove the redundant product
+                messages.success(request,("There is no such Item"))
         return Product.objects.filter(featured=True)
