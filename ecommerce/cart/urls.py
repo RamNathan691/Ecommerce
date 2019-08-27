e90863333f599e0efde48b4b8d33c639b3efe763
RamNathan691 <ramnathan0691@gmail.com>
2019-08-27 19:06:49 +0530

    done with the cart model

--- ecommerce/cart/urls.py
+++ ecommerce/cart/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from .views import cart_home,cart_update
 urlpatterns = [
   
-    path('',cart_home, name= "carthome"),
+    path('carthome',cart_home, name= "carthome"),
     path('update',cart_update, name= "cartupdate"),
 ]
