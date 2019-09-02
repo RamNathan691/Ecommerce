83325d596974e4de20fca56337dae7dac6a35330
RamNathan691 <ramnathan0691@gmail.com>
2019-09-02 17:46:58 +0530

    CREATED THE MODELS FOR THE ADDRESS

--- ecommerce/adress/models.py
+++ ecommerce/adress/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from billing.models import Billingprofile
 
-# Create your models here.
+ADDRESS_TYPES=(
+    ('billing','Billing'),
+    ('shipping','Shipping')
+)
+class Address(models.Model):
+    billing_profile=models.ForeignKey(Billingprofile,on_delete=models.CASCADE)
+    addressType=models.CharField(max_length=120,choice=ADDRESS_TYPES)
+    addressline1=models.CharField(max_length=120)
+    addressline2=models.CharField(max_length=120,null=True,blank=True)
+    city=models.CharField(max_length=120)
+    state=models.CharField(max_length=120)
+    country=models.CharField(max_lemgth=120,default="India")
+    postalcode=models.CharField(max_length=120)
+    
+
+    def __str__(self):
+        return self.billing_profile
