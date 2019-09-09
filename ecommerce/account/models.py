63d6ff6c424457b38aa78d5b01c0265b6c8a4273
RamNathan691 <ramnathan0691@gmail.com>
2019-09-09 17:59:24 +0530

    Creating a Custom user model

--- ecommerce/account/models.py
+++ ecommerce/account/models.py
@@ -1,6 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import(
+    AbstractBaseUser,BaseUserManager
+)
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,is_staff=False,is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user=self.model(
+            email=self.normalize_email(email)
+
+        )
+        user.set_password(password)#it is also used to change the user password
+        user.save(using=self._db)
+        return user_obj
+
+
+
 class GuestEmail(models.Model):
     email=models.EmailField()
     active=models.BooleanField(default=True)
@@ -9,3 +25,37 @@ class GuestEmail(models.Model):
 
     def __str__(self):
      return self.email
+
+class User(AbstractBaseUser):
+    email=models.EmailField(max_length=255,unique=True)
+    active=models.BooleanField(default=True)# can login
+    staff=models.BooleanField(default=False)# staff user non superuser
+    admin=models.BooleanField(default=False)# superuser
+    timestamp=models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD='email'#this is the goona be the default username
+    objects=UserManager()
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email 
+     
+
+    
+    @property
+    
+    def is_staff(self):
+        return self.staff
+    
+    @property
+    
+    def is_admin(self):
+        return self.admin
+    
+    @property
+    
+    def is_active(self):
+        return self.active
