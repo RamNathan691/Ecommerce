e84bd17ab148f0d4c4075a6f994ce54581644a86
RamNathan691 <ramnathan0691@gmail.com>
2019-10-10 11:32:30 +0530

    Loginpage is converted into the loginview

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from account.views import RegisterView
+from account.views import RegisterView,LoginView
 from django.contrib.auth.views import LogoutView
 urlpatterns = [
     #path('', views.homepage,name="homepage"),
     path('contact/',views.contactpage,name="contact"),
-    path('login/',views.loginpage,name="login"),
+    path('login/',LoginView.as_view(),name="login"),
      path('logout/',LogoutView.as_view(),name="logout"),
     path('register/',RegisterView.as_view(),name="register"),
     path('register/guest',views.guest_page,name="guest")
--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -35,18 +35,17 @@ def guest_page(request):
     return redirect("register")
 class LoginView(FormView):
       form=LoginForm
-      
+      success_url='/'
+      template_name="auth/loginpage.html"
 
-def loginpage(request):
-    login_form=LoginForm(request.POST or None)
-    next_=request.GET.get('next')
-    next_post=request.POST.get('next')
-    redirect_path=next_ or next_post or None
-    if login_form.is_valid():
-        username = login_form.cleaned_data.get("username")
-        password =login_form.cleaned_data.get("password")
+      def form_valid(self,form):
+        request=self.request
+        next_=request.GET.get('next')
+        next_post=request.POST.get('next')
+        redirect_path=next_ or next_post or None
+        username = form.cleaned_data.get("username")
+        password =form.cleaned_data.get("password")
         user = authenticate(request,username=username,password=password)
-        print(user)
         if user is not None:
             login(request,user)  
             try:
@@ -57,9 +56,31 @@ def loginpage(request):
                 return redirect(redirect_path)
             else:
                    messages.success(request,("Invalid Login Navigation "))
-        else:
-              messages.success(request,("Your username is not valid"))
-    return render(request,"auth/loginpage.html",{"form":login_form})
+        return super(LoginView,self).form_invalid(form)
+
+#def loginpage(request):
+ #   login_form=LoginForm(request.POST or None)
+  #  next_=request.GET.get('next')
+   # next_post=request.POST.get('next')
+    #redirect_path=next_ or next_post or None
+   # if login_form.is_valid():
+    #    username = login_form.cleaned_data.get("username")
+     #   password =login_form.cleaned_data.get("password")
+      #  user = authenticate(request,username=username,password=password)
+       # print(user)
+        #if user is not None:
+         #   login(request,user)  
+          #  try:
+           #     del request.session['guest_email']
+            #except:
+             #       pass
+            #if is_safe_url(redirect_path,request.get_host()):
+             #   return redirect(redirect_path)
+            #else:
+             #      messages.success(request,("Invalid Login Navigation "))
+        #else:
+         #     messages.success(request,("Your username is not valid"))
+    #return render(request,"auth/loginpage.html",{"form":login_form})
 User=get_user_model()
 class RegisterView(CreateView):
     form=RegisterForm
