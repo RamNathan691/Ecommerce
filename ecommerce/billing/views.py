37c7c32493476e602df5d0629c1e08687c292e50
RamNathan691 <ramnathan0691@gmail.com>
2019-10-11 11:17:42 +0530

    creating the  form to get the mode of payment either credit or debit card and creating a form for that claed quickstartelement site

--- ecommerce/billing/views.py
+++ ecommerce/billing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+import stripe
+stripe.api_key="sk_test_2GsGy0BJZie3PWJQfSXWUOzF00TCAcAgtr"
+STRIPE_PUB_KEY='pk_test_fG9TWih683sQBIqnZmitA9f800WJ06GXpE'
+def payment_method_view(request):
+    if request.method =="POST":
+        print(request.POST)
+    return render(request,'billing/payment-method.html',{"publish_key":STRIPE_PUB_KEY})
