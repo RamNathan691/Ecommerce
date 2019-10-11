b001d2f78a8e5191645ad95faea25af6018f7626
RamNathan691 <ramnathan0691@gmail.com>
2019-10-11 10:43:22 +0530

    started with stripe intergrations

--- ecommerce/billing/models.py
+++ ecommerce/billing/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_save
 User=settings.AUTH_USER_MODEL
 # Create your models here.
 import stripe
